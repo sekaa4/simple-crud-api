baab0407e2ca235e89e25dd36714f09b4f098270
Sergey P <s.pansevi4@gmail.com>
2023-07-02 18:43:44 +0300

    feat: add constants.ts file

---
+++ src/types/constants.ts
@@ -0,0 +1,38 @@
+export enum Endpoints_CONSTANTS {
+  DEFAULT = '/',
+  USERS = '/api/users',
+  USER_ID = '/api/users/',
+}
+
+export enum Methods {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+export const Status_CODE = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  NOT_IMPLEMENTED: 501,
+  SERVICE_UNAVAILABLE: 503,
+  BAD_GATEWAY: 502,
+} as const;
+
+export const Status_Message = {
+  OK_MESSAGE: 'Success',
+  BAD_REQUEST_MESSAGE: 'Bad Request',
+  UNAUTHORIZED_MESSAGE: 'Unauthorized',
+  FORBIDDEN_MESSAGE: 'Forbidden',
+  NOT_FOUND_MESSAGE: 'Not Found',
+  INTERNAL_SERVER_ERROR_MESSAGE: 'Internal Server Error',
+  NOT_IMPLEMENTED_MESSAGE: 'Not Implemented',
+  SERVICE_UNAVAILABLE_MESSAGE: 'Service Unavailable',
+  BAD_GATEWAY_MESSAGE: 'Bad Gateway',
+} as const;
